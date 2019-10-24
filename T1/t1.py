5d62c648c2169186bae8becff35c9b6d7e0585e2
AndersonMontor <ander_1434@hotmail.com>
2019-10-24 11:45:27 -0200

    algumas alteracoes T1

--- T1/t1.py
+++ T1/t1.py
@@ -11,7 +11,30 @@ def busca_arestas(v_arestas, origem):
 			resultado.append(a)
 	return resultado
 
-# Retorna a matriz pagerank a partir de uma matriz e fator "a" dados
+# Constroi matriz de transições, considerando escadas e cobras
+# n: total de casas do tabuleiro
+# arestas_especiais: conjuntos das arestas cobras e escadas
+def matrizP_snakesladders(n, arestas_especiais):
+	P = np.zeros((n,n))
+
+	for i in range(n-2):
+		for j in range(1,3): # j = 1 e 2
+			especial = busca_arestas(arestas_especiais, i + j)
+			dest = i + j
+			if especial:
+				dest = especial[0][1]
+
+			P[i][dest] = 0.5
+			
+	# Como penultima casa só pode avançar pra ultima, tem probabilidade 100%
+	P[n-2][n-1] = 1.
+	# Ultima casa é absorvente, ou seja, transita 100% pra ele mesmo
+	P[n-1][n-1] = 1.
+
+	return P
+
+
+# Retorna a matriz pagerank a partir de uma matriz quadrada e fator "a" dados
 def matriz_pagerank(m_origem, a):
 	m_pagerank = np.copy(m_origem)
 	n = len(m_origem[1, :])
@@ -74,24 +97,10 @@ if custom_entradas:
 	k_pagerank = input("Iteracoes k do power method na matriz pagerank, k = ")
 	
 n = m ** 2
-P = np.zeros((n,n))
 arestas_especiais = arestas_ladders + arestas_snakes
 
-# Constroi matriz de transições, considerando escadas e cobras
-for i in range(n-2):
-	for j in range(1,3): # j = 1 e 2
-		especial = busca_arestas(arestas_especiais, i + j)
-		dest = i + j
-		if especial:
-			dest = especial[0][1]
-
-		P[i][dest] = 0.5
-		
-# Como penultima casa só pode avançar pra ultima, tem probabilidade 100%
-P[n-2][n-1] = 1.
-# Ultima casa é absorvente, ou seja, transita 100% pra ele mesmo
-P[n-1][n-1] = 1.
-
+# Gera matriz P e probabilidade inicial w0
+P = matrizP_snakesladders(n, arestas_especiais)
 w0 = np.zeros(n)
 w0[0] = 1. # Estado inicial, começa na primeira casa do tabuleiro
 	
